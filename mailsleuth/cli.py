a1e8066d5ae3bed4578022e32b38f4e2975585f6
Macadangit <macadrian20@gmail.com>
2025-08-21 15:40:05 -0700

    Removed mailsleuth/mailsleuth.py and updated __init__.py to reflect changes in the project structure.

--- mailsleuth/cli.py
+++ mailsleuth/cli.py
@@ -1,5 +1,6 @@
 """This module provides the mailsleuth CLI."""
 # mailsleuth/cli.py
+from email.parser import BytesParser
 
 from typing import Optional
 
@@ -9,9 +10,31 @@ from mailsleuth import __app_name__, __version__
 
 app = typer.Typer()
 
+#---- Commands -----
+
+def _read_eml(filepath: str)-> None:
+    with open(filepath, 'rb') as f:# Bytesparser only takes bytes - must be in bytes mode
+        parser = BytesParser()
+        header = parser.parse(f)# headeronly is true by default. Creates email object here
+        print(header)#for testing
+
+
+
+
+
+
+
+#---- Callbacks ----
+
 def _version_callback(value: bool) -> None:
+    logo = (r"""               .__.__         .__                 __  .__     
+  _____ _____  |__|  |   _____|  |   ____  __ ___/  |_|  |__  
+ /     \\__  \ |  |  |  /  ___/  | _/ __ \|  |  \   __\  |  \ 
+|  Y Y  \/ __ \|  |  |__\___ \|  |_\  ___/|  |  /|  | |   Y  \
+|__|_|  (____  /__|____/____  >____/\___  >____/ |__| |___|  /
+      \/     \/             \/          \/                 \/ """)
     if value:
-        typer.echo(f"{__app_name__} v{__version__}")
+        typer.echo(f"{logo} \n \n {__app_name__} v{__version__} \n")
         raise typer.Exit()
 
 @app.callback()
