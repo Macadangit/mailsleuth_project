632e5bb21605dc8ef127b4f319f184549353e8ab
Macadangit <macadrian20@gmail.com>
2025-08-20 17:25:27 -0700

    Creating the basic structure for the mailsleuth project.

---
+++ mailsleuth/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for mailsleuth."""
+# rptodo/__init__.py
+
+__app_name__ = "mailsleuth"
+__version__ = "0.1.0"
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+) = range(3)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+}
---
+++ mailsleuth/__main__.py
@@ -0,0 +1,10 @@
+"""Mailsleuth entry point script."""
+# rptodo/__main__.py
+
+from mailsleuth import cli, __app_name__
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
